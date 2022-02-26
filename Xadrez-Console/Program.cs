2cf708b6d108badf92feddbb7fc4f3c9201e4512
Thales Arêde <thales_arede@hotmail.com>
2022-02-26 15:10:18 -0300

    Criada a classe prosição

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-
-namespace Xadrez-Console
+using tabuleiro; // Adicionamos o using tabuleiro
+namespace Xadrez_Console
 {
     class Program
     {
     static void Main(string[] args)
         {
 
-            
+        Posicao P; // Variável do tipo posicao
 
+        P = new Posicao(3, 4); // novo objeto posicao
+
+
+        Console.WriteLine("Posição: " + P);
+
+        Console.ReadLine(); // ReadLine adicionado para que espere apertarmos o "Enter"
         }
     }
 }
---
+++ Xadrez-Console/tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿namespace tabuleiro
+{
+    internal class Posicao
+    {
+        public int linha { get; set; } // Encapsulamento, atributos podem ser acessado e alterados por outras classes
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna) // Construtor, atribui os valores diretamente para objetos instanciados
+        {
+            this.linha = linha; // A palavra-chave this é usada para acessar um membro da instância da própria classe
+            this.coluna = coluna;
+        }
+
+        public override string ToString() // Método ToString para converter o objeto para string
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
